7e4321446bedf6d2777a9c1c538d20fc34fc18cc
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-03-31 19:20:11 +0600

    copy

--- src/component/Post/Post.js
+++ src/component/Post/Post.js
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Post = ({message}) =>{
+    const [posts, setPosts] = useState([]);
     console.log("[Post.js] render");
 
     // Выводится только при первом выводе
@@ -13,13 +15,28 @@ const Post = ({message}) =>{
         console.log("[Post.js] render with useEfect deprding on message varible")
     }, [message]);
 
-    useEffect(() =>{
-        setTimeout(() =>{
-            alert("Hello form Post")
-        }, 200)
-    }, [message])
+    // useEffect(() =>{
+    //     setTimeout(() =>{
+    //         alert("Hello form Post")
+    //     }, 200)
+    // }, [message]);
+
+     console.log("test");
+     axios.get("https://jsonplaceholder.typicode.com/posts'")
+     .then((response) =>{
+         setPosts(response.data)
+     })
+     console.log("test")
+
+
+
     return(
-        <div></div>
+        <div>
+            <article>
+                <h1>tile</h1>
+                <p>body</p>
+            </article>
+        </div>
     )
 }
 export default Post
