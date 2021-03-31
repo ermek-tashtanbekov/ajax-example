cbfbef3074c1dee3c2fe950d509d7863a28a0fa8
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-03-31 21:44:48 +0600

    added style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Posts from './component/Post/Posts';
 
 function App() {
+  const [message, setMessage] = useState("Hello");
+  const  [selectedPost, setSelectedPost] = useState(0);
+  console.log("[App.js] render" )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {message}
+     <button onClick={() =>setMessage("By")}>Toogle</button>
+     <Posts setSelectedPost={setSelectedPost} message={message}/>
     </div>
   );
 }
